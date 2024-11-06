adc5d5e701cf2796d44a020538cb86315b640438
mj <minai621@naver.com>
2024-11-06 11:51:56 +0900

    Remove unused import and update module exports

--- app/langchain/__init__.py
+++ app/langchain/__init__.py
@@ -1,5 +1,3 @@
 # app/langchain/__init__.py
-
-from .chains import create_overall_chain
 from .prompts import QUIZ_PROMPT, SUMMARY_PROMPT
 __all__ = ["create_overall_chain", "QUIZ_PROMPT", "SUMMARY_PROMPT"]
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 # app/models/__init__.py
 from .quiz_models import Quiz, QuizResponse
-from .document_models import DocumentUploadResponse
 
-__all__ = ["Quiz", "QuizResponse", "DocumentUploadResponse"]
+__all__ = ["Quiz", "QuizResponse"]
